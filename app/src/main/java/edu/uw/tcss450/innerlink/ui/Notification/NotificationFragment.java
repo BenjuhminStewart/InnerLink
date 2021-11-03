d13c8ece1f4e704b20da5b516dea05881a8f0ec4
Alexander Paule <alec.paule@yahoo.com>
2021-11-02 21:03:35 -0700

    Adds basic recycler view for notifications

--- app/src/main/java/edu/uw/tcss450/innerlink/ui/Notification/NotificationFragment.java
+++ app/src/main/java/edu/uw/tcss450/innerlink/ui/Notification/NotificationFragment.java
@@ -2,6 +2,8 @@ package edu.uw.tcss450.innerlink.ui.Notification;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,17 +11,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.innerlink.R;
+import edu.uw.tcss450.innerlink.databinding.FragmentChatBinding;
+import edu.uw.tcss450.innerlink.ui.Chat.ChatFragmentArgs;
 
 public class NotificationFragment extends Fragment {
-
-    public NotificationFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ChatFragmentArgs args = ChatFragmentArgs.fromBundle(getArguments());
+
+        FragmentChatBinding binding = FragmentChatBinding.bind(requireView());
+
+        binding.textSender.setText(args.getChat().getSender());
+        binding.textPreview.setText(args.getChat().getMessage());
     }
 }
