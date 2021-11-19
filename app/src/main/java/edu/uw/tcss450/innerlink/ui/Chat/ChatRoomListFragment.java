08dceb9d7ab0d3944900c06d9d64fa91fb935d2e
Alex Perez <alexperez761@gmail.com>
2021-11-19 02:02:14 -0800

    ChatRoomListFragment update
    Made changes to add the adapter to the recycler view, no longer says "no adapter, cannot display layout" anymore.

--- app/src/main/java/edu/uw/tcss450/innerlink/ui/Chat/ChatRoomListFragment.java
+++ app/src/main/java/edu/uw/tcss450/innerlink/ui/Chat/ChatRoomListFragment.java
@@ -6,10 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.uw.tcss450.innerlink.R;
 import edu.uw.tcss450.innerlink.databinding.FragmentChatRoomListBinding;
@@ -46,7 +52,22 @@ public class ChatRoomListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat_room_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_chat_room_list, container, false);
+
+        rootView.setTag("RecyclerViewFragment");
+        RecyclerView recycler = (RecyclerView) rootView.findViewById(R.id.list_root);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recycler.setLayoutManager(layoutManager);
+        // TODO: "E/Populate chatIds list with actual chatId values that the user has.
+        List chatIds = new ArrayList<>();
+        chatIds.add(HARD_CODED_CHAT_ID);
+
+        ChatRoomRecyclerViewAdapter adapter = new ChatRoomRecyclerViewAdapter(chatIds);;
+        recycler.setAdapter(adapter);
+
+        return rootView;
     }
 
     @Override
